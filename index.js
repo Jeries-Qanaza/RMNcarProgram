aef304bafbd2e2e654719e13661e1e29903d8194
jerry <85928945+Jeries-Qanaza@users.noreply.github.com>
2023-12-18 11:10:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const http = require('http');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
+
+let isOccupied = false;
+let booking_data;
+
 const app = express();
 
 app.use(bodyParser.json());
 
-let isCarOccupied = false;
-
 app.get('/car-status', (req, res) => {
-  res.json({ isOccupied: isCarOccupied });
+  res.json({ isOccupied: isOccupied });
 });
 
 app.post('/update-car-status', (req, res) => {
-  isCarOccupied = req.body.isOccupied;
+  isOccupied = req.body.isCarOccupied;
+  booking_data = req.body.booking;
+  console.log("Start Data from Server");// to del
+  console.log(booking_data);//
+  console.log("End Data from Server");//
   res.send('Car status updated successfully.');
 });
 
